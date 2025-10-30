b1c4e978584033a4457a3bb2e144e5942650ad51
hassan <hassona1392004@gmail.com>
2025-10-30 14:16:54 +0300

    made the links

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const burgerMenuIcon = document.querySelector("nav img");
 const navMenu = document.querySelector("nav ul");
+const homeNavigatorOnLi = document.getElementById("homeNavigator");
+const currentPage = window.location.pathname.split("/").pop().split(".")[0];
 
 burgerMenuIcon.onclick = () => {
     navMenu.classList.toggle("hidden");
 };
+
+if (currentPage == "index")
+    homeNavigatorOnLi.href = "#header";
